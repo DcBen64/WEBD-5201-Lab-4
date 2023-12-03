b495c54b3afc5331284610f28d96aa58dcda2123
Benjamin Walsh <benjamin.walsh1@dcmail.ca>
2023-12-03 03:32:54 -0500

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/home_controller.rb
+class HomeController < ApplicationController
+  def index
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
+  end
+end
